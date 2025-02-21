e357b862fed96126b6fdc09d6886b5959a96f765
Odili1 <ezedigwegerald1@gmail.com>
2025-02-21 21:33:53 +0100

    chore: testing

--- src/middleware/rateLimit.middleware.ts
+++ src/middleware/rateLimit.middleware.ts
@@ -1,21 +1,4 @@
 import express from "express";
-// import rateLimit from "express-rate-limit";
-
-// const rateLimiter = rateLimit({
-//   windowMs: 60 * 1000,
-//   max: 1,
-//   message: "Time Out",
-//   headers: true,
-// });
-
-// export default rateLimiter;
-// JSON.stringify({
-//   status: "error",
-//   message:
-//     'Invalid Prompt. \nMessage Prompt - Should be in this format: "/slack-[slack channel]"',
-//   event_name: `Slack Messenger Error`,
-//   username: "Slack Alert",
-// });
 
 interface RequestTracker {
   count: number;
